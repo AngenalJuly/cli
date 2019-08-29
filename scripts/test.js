60ed06622d4f78efd3b0f79ff10f02936100b8ae
zhangfeiyang <zhangfy@qtrade.com>
2019-08-29 16:58:09 +0800

    优化

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,5 @@
 const download = require('download-git-repo')
 const href = 'https://github.com:AngenalJuly/react-mpa#master'
-console.log(href)
 const action = () => {
     download(href, './aaa', { clone: true }, err => {
         console.log(err)
